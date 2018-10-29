06e3649fdb726f50bda5cfd675d5791e73ebae4c
SuchitraaKoganti <suchitraakoganti@gmail.com>
2018-10-29 14:19:41 +0530

    Add files via upload

---
+++ Databaseconnectivitysampledemoquery.sql
@@ -0,0 +1,30 @@
+create table mohanapriya.DBemployees
+(
+empName varchar(30),
+empID int,
+empCity varchar(30),
+empPhone bigint,
+empDOJ datetime
+)
+
+insert into mohanapriya.DBemployees values ('Sam','11','chennai','9876576878','03/10/1998')
+
+insert into mohanapriya.DBemployees values ('Jesus','12','chennai','9876678378','11/08/1996')
+
+insert into mohanapriya.DBemployees values ('Abi','13','mumbai','9324576878','03/12/1997')
+
+insert into mohanapriya.DBemployees values ('Ravi','14','pune','9876523465','10/08/1996')
+
+insert into mohanapriya.DBemployees values ('Janani','15','kolkata','9239876878','03/10/1996')
+
+insert into mohanapriya.DBemployees values ('Sonam','16','delhi','9230876878','08/04/1999')
+
+insert into mohanapriya.DBemployees values ('Meera','17','chennai','9873096878','01/11/1998')
+
+insert into mohanapriya.DBemployees values ('Bala','18','bangalore','9879976888','07/10/1999')
+
+insert into mohanapriya.DBemployees values ('Ambiga','19','delhi','9876575656','12/12/1996')
+
+insert into mohanapriya.DBemployees values ('Monisha','20','pune','9843276878','11/10/1998')
+
+select * from mohanapriya.DBemployees
